0e3228c0c7545f702a755afcb26cdab81f5589cb
Rivero, Franco <francorivero2012@gmail.com>
2021-05-08 15:21:55 -0300

    Add some improvements to deploy_environment script and freebsd_base.json

--- deploy_environment.sh
+++ deploy_environment.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-
-HOMEPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
-read -p "Enter version of FreeBSD: " VERSION
-ISO_NAME="FreeBSD-$VERSION-RELEASE-amd64-dvd1.iso"
-packerConfig=freebsd_base.json
-packerConfig_AUX=freebsd.json
-
+typeset -A config 
+config=(
+    [VERSION]="13.0"
+    [ISO_NAME]="FreeBSD-${config[VERSION]}-RELEASE-amd64-dvd1.iso"
+    [HOME]="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+    [PACKER_CONFIG_BASE]="freebsd_base.json"
+    [PACKER_CONFIG]="freebsd.json"
+)
 detect_os_and_install_dependecies() {
     OS=$(hostnamectl | grep "Operating System:" )
     if [[ $(echo \"$OS\" | grep "Ubuntu") ]]; then
-        $HOMEPATH/dependencies_installers/install_ubuntu_dependencies.sh
+        ${config["HOME"]}/dependencies_installers/install_ubuntu_dependencies.sh
     elif [[ $(echo \"$OS\" | grep "Red Hat") ]]; then
         echo "$OS"
     elif [[ $(echo \"$OS\" | grep "Debian") ]]; then
@@ -26,45 +27,44 @@ main() {
     detect_os_and_install_dependecies
 
     #Create folder if it doesn't exist
-    if [ ! -d $HOMEPATH/http ]; then
-        mkdir -p $HOMEPATH/http
+    if [ ! -d ${config["HOME"]}/http ]; then
+        mkdir -p ${config["HOME"]}/http
     fi
 
     #Get FreeBSD iso
-    if [ ! -f $HOMEPATH/http/$ISO_NAME ]; then
-        iso_freebsd_path=$(sudo find / -name "$ISO_NAME" -print | head -n 1)
+    if [ ! -f ${config["HOME"]}/http/${config["ISO_NAME"]} ]; then
+        iso_freebsd_path=$(sudo find / -name "${config["ISO_NAME"]}" -print | head -n 1)
         if [[ "$iso_freebsd_path" == "" ]]; then
-            wget https://download.freebsd.org/ftp/releases/amd64/amd64/ISO-IMAGES/$VERSION/$ISO_NAME
-        else
-            mv $iso_freebsd_path $HOMEPATH/http/$ISO_NAME
-        fi
+            wget https://download.freebsd.org/ftp/releases/amd64/amd64/ISO-IMAGES/${config["VERSION"]}/${config["ISO_NAME"]}
+        fi 
+        mv $iso_freebsd_path ${config["HOME"]}/http/${config["ISO_NAME"]}
     fi
 
     #Evaluate path
     output_path=$(sudo find / -type d -name "VirtualBox VMs")
 
-    cp $packerConfig $packerConfig_AUX
-    sed -i 's|ISO_NAME|'$ISO_NAME'|g' $packerConfig_AUX
-    ISOSHA2=$(sha256sum $HOMEPATH/http/$ISO_NAME | awk '{print $1}')
-    sed -i 's|ISO_CHECKSUM|'$ISOSHA2'|g' $packerConfig_AUX
+    cp ${config["PACKER_CONFIG_BASE"]} ${config["PACKER_CONFIG"]}
+    sed -i 's|ISO_NAME|'${config["ISO_NAME"]}'|g' ${config["PACKER_CONFIG"]}
+    ISOSHA2=$(sha256sum ${config["HOME"]}/http/${config["ISO_NAME"]} | awk '{print $1}')
+    sed -i 's|ISO_CHECKSUM|'$ISOSHA2'|g' ${config["PACKER_CONFIG"]}
 
     #Validate and build VM with Packer
-    packer validate $packerConfig_AUX
+    packer validate ${config["PACKER_CONFIG"]}
     retVal=$?
     if [ $retVal -ne 0 ]; then
         echo "Validation fails"
         exit $retVal
     fi
 
-    packer build -var output_path="$output_path" $packerConfig_AUX
+    packer build -var output_path="$output_path" ${config["PACKER_CONFIG"]}
 
     #Clean output
-    if [[ -d "$HOMEPATH/packer_cache" ]]; then
+    if [[ -d "${config["HOME"]}/packer_cache" ]]; then
         rm -rf packer_cache
     fi
 
-    if [[ -f "$packerConfig_AUX" ]]; then
-        rm $packerConfig_AUX
+    if [[ -f "${config["PACKER_CONFIG"]}" ]]; then
+        rm ${config["PACKER_CONFIG"]}
     fi
 }
 
