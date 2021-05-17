3e8706cd3ad07f9d39aa3ebfdf08530bb38f94d4
Rivero, Franco <francorivero2012@gmail.com>
2021-05-16 23:43:24 -0300

    Add information messages and virtualbox installation

--- dependencies_installers/install_ubuntu_dependencies.sh
+++ dependencies_installers/install_ubuntu_dependencies.sh
@@ -1,23 +1,85 @@
 #!/bin/bash
+LOG_FILE="$1/installation_dependencies.log"
 
-#Declare constants
-HOMEPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P)"
+sudo apt update &>> $LOG_FILE
+cat $LOG_FILE | grep "Failed" >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo -e "\e[31m\e[1m\tRepository update failed\e[0m"
+    exit 1
+fi
+echo "------------------------------------------------------------------------------------------------------" >> $LOG_FILE
+echo "Repositories updated" >> $LOG_FILE
+echo -e "\e[32m\tRepositories updated\t\t\e[1m[X]\e \e[0m"
 
-#Install dependencies
-sudo apt update
-sudo apt upgrade -y
+sudo apt upgrade -y &>> $LOG_FILE
+cat $LOG_FILE | grep "Failed" >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo -e "\e[31m\e[1m\tPackages update failed\e[0m"
+    exit 1
+fi
+echo "------------------------------------------------------------------------------------------------------" >> $LOG_FILE
+echo "Packages updated" >> $LOG_FILE
+echo -e "\e[32m\tPackages updated\t\t\e[1m[X]\e \e[0m"
+
 sudo apt-get install -y linux-headers-$(uname -r)\
                         build-essential\
                         perl\
+                        gcc\
+                        make\
                         dkms\
                         sed\
                         wget\
                         virtualbox\
-                        network-manager
+                        network-manager &>> $LOG_FILE
+
+cat $LOG_FILE | grep "Failed" >/dev/null 2>&1
+if [ $? -eq 0 ]; then
+    echo -e "\e[31m\e[1m\tInstall dependencies failed\e[0m"
+    exit 1
+fi
+echo "------------------------------------------------------------------------------------------------------" >> $LOG_FILE
+echo "Dependencies were installed" >> $LOG_FILE
+echo -e "\e[32m\tDependencies\t\t\t\e[1m[X]\e \e[0m"
+
+#Update Virtualbox
+if [[ -z $(sudo apt list -a virtualbox 2>/dev/null | grep "installed") ]];then
+    VB_CURRENT_VERSION=$(VBoxManage --version)
+    if [[ $? != 0 ]];then
+        VB_CURRENT_VERSION=6.0
+    else
+        VB_CURRENT_VERSION=${VB_CURRENT_VERSION:0:3}
+    fi
+    VERSION_VB=6.1
+    if (( ${VB_CURRENT_VERSION%%.*} < ${VERSION_VB%%.*} || ( ${VB_CURRENT_VERSION%%.*} == ${VERSION_VB%%.*} && ${VB_CURRENT_VERSION##*.} < ${VERSION_VB##*.} ) )) ; then    
+        sudo apt remove virtualbox
+        wget -q https://www.virtualbox.org/download/oracle_vbox_2016.asc -O- | sudo apt-key add -
+        wget -q https://www.virtualbox.org/download/oracle_vbox.asc -O- | sudo apt-key add -
+        sudo add-apt-repository "deb [arch=amd64] http://download.virtualbox.org/virtualbox/debian $(lsb_release -cs) contrib"
+        sudo apt update
+        sudo apt-get install virtualbox-6.1
+        wget https://download.virtualbox.org/virtualbox/6.1.22/Oracle_VM_VirtualBox_Extension_Pack-6.1.22.vbox-extpack
+        VBoxManage extpack install Oracle_VM_VirtualBox_Extension_Pack-6.1.22.vbox-extpack
+    fi
+    if [[ -z $(sudo apt list -a virtualbox 2>/dev/null | grep "installed") ]];then
+        echo -e "\e[31m\e[1m\tInstall VirtualBox failed\e[0m"
+        exit 1
+    fi
+    echo "------------------------------------------------------------------------------------------------------" >> $LOG_FILE
+    echo "Virtualbox was installed" >> $LOG_FILE
+fi
+echo -e "\e[32m\tVirtualbox\t\t\t\e[1m[X]\e \e[0m"
 
 #Install Packer
 if [[ -z $(sudo apt list -a packer 2>/dev/null | grep "installed") ]]; then
-    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-    sudo apt-get update && sudo apt-get install packer
+    curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add - &>> $LOG_FILE
+    sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"  >> $LOG_FILE
+    sudo apt-get update &>>$LOG_FILE
+    sudo apt-get install packer &>> $LOG_FILE
 fi
+if [[ -z $(sudo apt list -a packer 2>/dev/null | grep "installed") ]]; then
+    echo -e "\e[31m\e[1m\tInstall packer failed\e[0m"
+    exit 1
+fi
+echo "------------------------------------------------------------------------------------------------------" >> $LOG_FILE
+echo "Packer was installed" >> $LOG_FILE
+echo -e "\e[32m\tPacker\t\t\t\t\e[1m[X]\e \e[0m"
--- deploy_environment.sh
+++ deploy_environment.sh
@@ -7,13 +7,15 @@ config=(
     [PACKER_CONFIG_BASE]="freebsd_base.json"
     [PACKER_CONFIG]="freebsd.json"
     [OUTPUT_PATH]=$(sudo find /home -type d -name "VirtualBox VMs")
-    [USER_GIT]="FrancoRivero"
+    [GIT_USER]="FrancoRivero"
+    [LOG_FOLDER]="/tmp/logs/log_$(date +%Y%m%d%H%M%s)"
+    [GIT_FOLDER]="~/repository"
 )
 
 detect_os_and_install_dependecies() {
     OS=$(hostnamectl | grep "Operating System:" )
     if [[ $(echo \"$OS\" | grep "Ubuntu") ]]; then
-        ${config["HOME"]}/dependencies_installers/install_ubuntu_dependencies.sh
+        ${config["HOME"]}/dependencies_installers/install_ubuntu_dependencies.sh ${config["LOG_FOLDER"]}
     elif [[ $(echo \"$OS\" | grep "Red Hat") ]]; then
         echo "$OS"
     elif [[ $(echo \"$OS\" | grep "Debian") ]]; then
@@ -29,51 +31,67 @@ detect_network_connection() {
     interfaces=$(nmcli device status | awk '{print $1}' | sed -n '1!p')
     for interface in $interfaces
     do
-        ping -I $interface -c 1 www.google.com >/dev/null 2>&1
-        cmd=$(echo $?)
-        if [[ $cmd == 0 ]];then
+        echo $interface >> ${config["LOG_FOLDER"]}/detect_network.log
+        echo "------------------------------------------------------------------------------------------------------" >> ${config["LOG_FOLDER"]}/detect_network.log
+        ping -I $interface -c 1 www.google.com >> ${config["LOG_FOLDER"]}/detect_network.log
+        if [[ $? == 0 ]];then
             network_output=$interface
+            return 0
         fi
     done
+    echo -e "\e[31m\e[1mNetwork detection failed\e[0m"
+    exit 1
 }
 
 main() {
+    echo -e "\e[32m\e[1mCreating development environment...\e[0m"
+    echo -e "\e[33m\e[1m--> Installing dependencies...\e[0m"
+    #Create log's folder
+    if [ ! -d /tmp/logs ]; then
+        mkdir -p /tmp/logs
+    fi
+    mkdir ${config["LOG_FOLDER"]}
+
     #Install dependecies
     detect_os_and_install_dependecies
+    if [ $? -ne 0 ]; then
+        echo -e "\e[31m\e[1mInstall dependencies failed\e[0m"
+        exit 1
+    fi
 
     #Evaluate interfaces
+    echo -e "\e[33m\e[1m--> Network detection...\e[0m"
     detect_network_connection
+    if [ $? -ne 0 ]; then
+        echo -e "\e[31m\e[1mNetwork detection failed\e[0m"
+        exit 1
+    fi
+    echo -e "\e[32m\tNetwork interface\t\t\e[1m[X]\e \e[0m"
 
     #Create folder if it doesn't exist
+    echo -e "\e[33m\e[1m--> Create environment to create a virtual machine...\e[0m"
     if [ ! -d ${config["HOME"]}/http ]; then
         mkdir -p ${config["HOME"]}/http
     fi
+    if [ $? -ne 0 ]; then
+        echo -e "\e[31m\e[1mhttp folder to create failed\e[0m"
+        exit 1
+    fi
+    echo -e "\e[32m\tHTTP folder\t\t\t\e[1m[X]\e \e[0m"
 
     #Get FreeBSD iso
     if [ ! -f ${config["HOME"]}/http/${config["ISO_NAME"]} ]; then
         iso_freebsd_path=$(sudo find /home -name "${config["ISO_NAME"]}" -print | head -n 1)
         if [[ "$iso_freebsd_path" == "" ]]; then
-            wget https://download.freebsd.org/ftp/releases/amd64/amd64/ISO-IMAGES/${config["VERSION"]}/${config["ISO_NAME"]}
+            wget https://download.freebsd.org/ftp/releases/amd64/amd64/ISO-IMAGES/${config["VERSION"]}/${config["ISO_NAME"]} >> ${config["LOG_FOLDER"]}/create_environment.log
         fi 
         mv $iso_freebsd_path ${config["HOME"]}/http/${config["ISO_NAME"]}
     fi
-
-    #Download repository
-    if [ ! -d ~/repository ]; then
-        mkdir ~/repository
+    if [ ! -f ${config["HOME"]}/http/${config["ISO_NAME"]} ]; then
+        echo -e "\e[31m\e[1mFreeBSD image download failed\e[0m"
+        exit 1
     fi
-    cd ~/repository
-    if [ ! -d ~/repository/freebsd-src ]; then
-        git clone git@github.com:${config["USER_GIT"]}/freebsd-src.git
-    fi
-    cd freebsd-src
-    git checkout develop_schedule
-    retVal=$?
-    if [ $retVal -ne 0 ]; then
-        git checkout release/${config["VERSION"]}.0
-        git switch -c develop_schedule
-    fi
-    repository_path=$(pwd)
+    echo -e "\e[32m\tFreeBSD image\t\t\t\e[1m[X]\e \e[0m"
 
     #Modify configuration file
     cd ${config["HOME"]}
@@ -83,14 +101,58 @@ main() {
     sed -i 's|ISO_CHECKSUM|'$ISOSHA2'|g' ${config["PACKER_CONFIG"]}
     sed -i 's|NETWORK_INTERFACE|'$network_output'|g' ${config["PACKER_CONFIG"]}
 
-    #Validate and build VM with Packer
-    packer validate ${config["PACKER_CONFIG"]}
-    retVal=$?
-    if [ $retVal -ne 0 ]; then
-        echo "Validation fails"
-        exit $retVal
+    #Validate configuration file
+    packer validate ${config["PACKER_CONFIG"]} >> ${config["LOG_FOLDER"]}/create_environment.log
+    if [ $? -ne 0 ]; then
+        echo -e "\e[31m\e[JSON validation failed\e[0m"
+        exit 1
     fi
-    packer build -var output_path="${config["OUTPUT_PATH"]}" ${config["PACKER_CONFIG"]}
+    echo -e "\e[32m\tConfiguration file\t\t\e[1m[X]\e \e[0m"
+    
+    #build VM with Packer
+    echo -e "\e[33m\e[1m--> Building virtual machine...\e[0m"
+    if [[ ! -d ${config["OUTPUT_PATH"]}/FreeBSD_for_testing ]];then
+        packer build -var output_path="${config["OUTPUT_PATH"]}" ${config["PACKER_CONFIG"]} >> ${config["LOG_FOLDER"]}/building_vm.log
+        if [ $? -ne 0 ]; then
+            echo -e "\e[31m\e[Build virtual machine failed\e[0m"
+            exit 1
+        fi
+    fi
+    echo -e "\e[32m\tBuild virtual machine\t\t\e[1m[X]\e \e[0m"
+
+    #Download repository
+    echo -e "\e[33m\e[1m--> Cloning freebsd repository and creating a branch since the last commit in the version ${config[VERSION]}...\e[0m"
+    if [ ! -d ~/repository ]; then
+        mkdir ~/repository
+    fi
+    cd ~/repository
+    if [ ! -d ~/repository/freebsd-src ]; then
+        git clone git@github.com:${config["GIT_USER"]}/freebsd-src.git &>> ${config["LOG_FOLDER"]}/freebsd_repository.log
+        if [ $? -ne 0 ]; then
+            echo -e "\e[31m\e[Clone freebsd repository failed\e[0m"
+            exit 1
+        fi
+    fi
+    echo -e "\e[32m\tRepository\t\t\t\e[1m[X]\e \e[0m"
+    
+    #Move to release tag
+    cd freebsd-src
+    if [[ -z $(git status | grep "freebsd_scheduler_develop") ]];then
+        git checkout freebsd_scheduler_develop &>> ${config["LOG_FOLDER"]}/freebsd_repository.log
+        if [ $? -ne 0 ]; then
+            git checkout release/${config["VERSION"]}.0 &>> ${config["LOG_FOLDER"]}/freebsd_repository.log
+            if [ $? -ne 0 ]; then
+                echo -e "\e[31m\e[Move to release tag failed\e[0m"
+                exit 1
+            fi
+            git switch -c freebsd_scheduler_develop &>> ${config["LOG_FOLDER"]}/freebsd_repository.log
+            if [ $? -ne 0 ]; then
+                echo -e "\e[31m\e[Create develop branch failed\e[0m"
+                exit 1
+            fi
+        fi
+    fi
+    echo -e "\e[32m\tBranch to develop\t\t\e[1m[X]\e \e[0m"
 
     #Clean output
     if [[ -d "${config["HOME"]}/packer_cache" ]]; then
@@ -100,6 +162,7 @@ main() {
     if [[ -f "${config["PACKER_CONFIG"]}" ]]; then
         rm ${config["PACKER_CONFIG"]}
     fi
+    echo -e "\e[32m\e[1mDevelopment environment ready to work!\e[0m"
 }
 
 main
